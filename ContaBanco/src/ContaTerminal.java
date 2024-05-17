8b51433458fdcb034eeb37ea99aa9c5fa3d46bc5
daniloterashima <dmterashima@gmail.com>
2024-05-17 02:34:12 -0300

    final_conta

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,10 +1,36 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+/*
+*@AUTHOR DANILO TERASHIMA 
+ * 
+*/
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-       int numero;
+       /*int numero;
        String agencia;
        String nomeCliente;
        double saldo;
+       */
+       Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+       System.out.println("Por favor, digite o número da Agência!");
+       int numero = scanner.nextInt();
+
+       System.out.println("Por favor, digite a Agência!");
+       String agencia = scanner.next();
+       
+       scanner.nextLine();
+
+       System.out.println("Por favor, digite seu nome!");
+       String nomeCliente = scanner.nextLine();
+
+       System.out.println("Por favor, digite seu saldo!");
+       double saldo = scanner.nextDouble();
+
+       System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + 
+       ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
 
        
+
     }
 }
