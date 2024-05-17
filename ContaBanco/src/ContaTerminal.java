71eb8ec17377e289735b780d03dafd8a884fb536
daniloterashima <dmterashima@gmail.com>
2024-05-17 01:41:07 -0300

    primeiro commit do desafio

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,10 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+       int numero;
+       String agencia;
+       String nomeCliente;
+       double saldo;
+
+       
+    }
+}
