e1d671d35eadcb2e5e5f44de325b1757907c3cd5
daniloterashima <dmterashima@gmail.com>
2024-05-23 22:12:41 -0300

    fim do desafio

---
+++ DesafiosJava/src/ControleSimplesDeSaques.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ControleSimplesDeSaques {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double limiteDiario = scanner.nextDouble();
+        
+        // Loop for para iterar sobre os saques
+        for (int i = 1; ; i++) {
+
+            double valorSaque = scanner.nextDouble();
+            double saldo = limiteDiario - valorSaque;
+            if (valorSaque == 0) {
+                System.out.println("Transacoes encerradas.");
+                break; // Encerra o loop
+            } else if (valorSaque < limiteDiario) {
+                System.out.println("Saque realizado. Limite restante: " + (saldo) + "\n" +
+                " Transacoes encerradas.");
+                break; // Encerra o loop
+            } else if (valorSaque > limiteDiario) {
+                System.out.println("Limite diario de saque atingido. Transacoes encerradas.");
+                break; // Encerra o loop
+            }
+        }
+
+        // Fechar o scanner para evitar vazamentos de recursos
+        scanner.close();
+    }
+}
