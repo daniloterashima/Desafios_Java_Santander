6452659a65725c3e645f07e15066d4272aa00ca6
Danilo M. Terashima da Silva <82854137+daniloterashima@users.noreply.github.com>
2024-07-21 22:04:01 -0300

    Add files via upload

---
+++ padroes-proj-spring/padroes-proj-spring/src/test/java/com/digitalone/padroes_proj_spring/PadroesProjSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalone.padroes_proj_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesProjSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
