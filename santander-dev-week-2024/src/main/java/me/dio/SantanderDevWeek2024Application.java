42cf1e8fd1ace8e24ba6728812c98c54e450ca08
Danilo M. Terashima da Silva <82854137+daniloterashima@users.noreply.github.com>
2024-07-27 22:17:03 -0300

    Add files via upload

---
+++ santander-dev-week-2024/src/main/java/me/dio/SantanderDevWeek2024Application.java
@@ -0,0 +1,14 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@author Danilo
+
+@SpringBootApplication
+public class SantanderDevWeek2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek2024Application.class, args);
+	}
+
+}
